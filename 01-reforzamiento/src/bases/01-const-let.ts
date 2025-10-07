a1d486ed58a8e4dc79e1c1cec9ee96ce2349c0df
gloozard <gloosard@gmail.com>
2025-10-06 22:21:51 -0600

    inicialice un projecto con vite y npm

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+const firstName: string= 'Emiliano';
+const lastName = 'Kick';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+console.log(firstName,lastName);
+
+const containsLetterE = lastName.includes('E')
+
+console.log({containsLetterE}, diceNumber)
---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,6 @@
+const firstName = "Emiliano";
+const lastName = "O'Neal";
+
+const fullname =`${firstName} ${lastName} ${1+1}`
+
+console.log(fullname);
